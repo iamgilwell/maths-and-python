d343ac41f0c7cd15872d86d6707df798d83c1ac5
Gilwell Muhati <gilwell.muhati@tezzasolutions.com>
2019-07-09 09:04:31 +0300

    modified defination to register and added a loop

--- welcome.py
+++ welcome.py
@@ -2,24 +2,24 @@ import re
 ''' A python CLI application to send email to specific individuals '''
 
 
-def enter_email_address(emailAddress):
-   # emailAddress = input("Enter your name: ")
-    # print(inputEmail)
-
-    #print("Your Email is {}".format(inputEmail))
-
+def register(emailAddress, *args):
     match = re.match(
         '^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', emailAddress)
-
-    if match == None:
+    while match is None:
+        #
         print("Your Email is {}".format(emailAddress),
               "Is not a valid email, please try again")
-        emailAddress = input("Enter your name: ")
-        enter_email_address(emailAddress)
-        #raise ValueError('Bad Syntax')
-    else:
-        print("Welcome to the 21st, further information being sent to your email")
+        emailAddress = input("Please Enter Your Email Address Again: ")
+        break
+    # register(emailAddress, args)
+
+    # else:
+    print("Welcome to the 21st {}, {}".format(first_name, second_name),
+          "further information being sent to your email")
 
 
-emailAddress = input("Enter your name: ")
-enter_email_address(emailAddress)
+emailAddress = input("Enter your  Email Address: ")
+first_name = input("Enter Your First Name: ")
+second_name = input("Enter Your Second Name: ")
+args = [first_name, second_name]
+register(emailAddress, args)
