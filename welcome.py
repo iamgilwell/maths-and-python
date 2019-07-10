93c06aaf15344fb13f3b39166ec4cabf13b3d6de
Gilwell Muhati <gilwell.muhati@tezzasolutions.com>
2019-07-10 15:50:12 +0300

    Added Password 1 and Password2

--- welcome.py
+++ welcome.py
@@ -1,25 +1,38 @@
 import re
+import getpass
+
 ''' A python CLI application to send email to specific individuals '''
 
 
 def register(emailAddress, *args):
+    print(len(args))
     match = re.match(
         '^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', emailAddress)
+
     while match is None:
         #
         print("Your Email is {}".format(emailAddress),
               "Is not a valid email, please try again")
         emailAddress = input("Please Enter Your Email Address Again: ")
-        break
-    # register(emailAddress, args)
+        match = re.match(
+            '^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', emailAddress)
+        # register(emailAddress, args)
+  # print(args[2])
+
+    # if args['password'] == args['password2']:
+    #     print("Your password did not match, please try again")
+    #     password = getpass.getpass(prompt="Enter Password: ")
+    #     password2 = getpass.getpass(prompt="Confirm Password: ")
 
     # else:
     print("Welcome to the 21st {}, {}".format(first_name, second_name),
           "further information being sent to your email")
 
 
-emailAddress = input("Enter your  Email Address: ")
 first_name = input("Enter Your First Name: ")
 second_name = input("Enter Your Second Name: ")
-args = [first_name, second_name]
-register(emailAddress, args)
+emailAddress = input("Enter your  Email Address: ")
+password = getpass.getpass(prompt="Enter Password: ")
+password2 = getpass.getpass(prompt="Confirm Password: ")
+args = [first_name, second_name, password, password2]
+register(*args)
