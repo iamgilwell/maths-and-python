807eacbcef01f368ed3b9f6c1230476bce0081fa
Gilwell Muhati <gilwell.muhati@tezzasolutions.com>
2019-07-16 09:04:32 +0300

    Tkinter window intialized

--- welcome.py
+++ welcome.py
@@ -2,8 +2,25 @@ import re
 import getpass
 import psycopg2
 from datetime import datetime as dt
+from tkinter import *
 
-''' A python CLI application to send email to specific individuals '''
+from tkinter.ttk import *
+
+window = Tk()
+
+window.title("Welcome to LikeGeeks app")
+
+window.geometry('350x200')
+
+combo = Combobox(window)
+
+combo['values'] = (1, 2, 3, 4, 5, "Text")
+
+combo.current(1)  # set the selected item
+
+combo.grid(column=0, row=0)
+
+window.mainloop()
 
 
 def register(*args):
